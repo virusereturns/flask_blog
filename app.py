28a0dff32456952c5d7b86eff5cfd2ba7119e2af
Tanya <tanyatree.dev@gmail.com>
2024-03-17 17:29:54 -0300

    Added some of Corey's styles from the tutorial, and added support to change user pictures

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
+import os
+import secrets
+from PIL import Image
 from datetime import datetime, UTC
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import Flask, render_template, url_for, flash, redirect, request
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 from wtforms import ValidationError
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-from flask_login import LoginManager, UserMixin, login_user, current_user, logout_user
+from flask_login import LoginManager, UserMixin, login_user, current_user, logout_user, login_required
 
 
 # Configuration
@@ -19,6 +23,8 @@ bootstrap = Bootstrap5(app)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 
 # Models
@@ -80,6 +86,25 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Login', render_kw={"class": "btn btn-primary"})
 
 
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update', render_kw={"class": "btn btn-primary"})
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken. Please choose a different one.')
+
+
 # Routes
 
 
@@ -91,6 +116,7 @@ def home():
 
 @app.route('/about/')
 def about():
+    flash('This is a flash message', 'info')
     return render_template('about.html')
 
 
@@ -119,7 +145,8 @@ def login():
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             flash(f'Welcome {user.username}!', 'success')
-            return redirect(url_for('home'))
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash(f'Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', form=form)
@@ -131,5 +158,41 @@ def logout():
     return redirect(url_for('home'))
 
 
+def save_picture(form_picture: FileField) -> str:
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_filename = random_hex + f_ext
+    # app.root_path is the path to the root directory of the app
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_filename)
+    # Resize the image before saving it, using the PIL library
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    return picture_filename
+
+
+@app.route('/account/', methods=['GET', 'POST'])
+@login_required
+def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        # We are doing this to let SQLAlchemy know that we are updating the user
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            print(picture_file)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash(f'Your account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', image_file=image_file, form=form)
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5001)
